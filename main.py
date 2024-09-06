44e7f4d1b9d4665ae39b8c2ba900f9eecdf6f589
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2024-09-06 13:58:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,20 +14,24 @@ def send_slack_message(message, channel):
         client = WebClient(token=SLACK_TOKEN)
         client.chat_postMessage(channel=channel, text=message)
     except SlackApiError as e:
-        print(f"Error sending message to {channel} : {e}")
+        print(f"Error sending message to {channel} : {e}")0
 def main():
     for cluster in clusters:
         # 메시지 제목 설정
         header = f":loudspeaker: *『인사총무팀 공지』* <!channel>\n\n"
 
         notice_msg = (
-            f"안녕하세요? 평택 클러스터 구성원 여러분!\n평택 클러스터 6층 컬리스라운지 냉장고 사용 에티켓 안내드립니다.\n\n"
+            f"안녕하세요? 인사총무팀 *알리미 봇* 입니다!:smile:\n평택 클러스터 금일 *신규 입사자 사물함 배정*을 아래와 같이 공유 드립니다.\n\n"
             f"\n"
             f"\n"
-            f":k체크: *공용 사용 냉장고로 항상 깨끗하게 사용 부탁드립니다.*\n"
-            f":k체크: *위생관리를 위해 매주 금요일 오전11시 냉장고 내부상품을 폐기 합니다.*\n"
+            f":k체크: *락카룸 장소*는 6층에 있습니다.\n"
+            f":k체크: 락카룸 배정 후 *자물쇠는 본인 지참*부탁드립니다.\n"
+            f":k체크: 락카룸 내부는 *CCTV 미 설치 구역*으로 꼭! 자물쇠를 이용하여 시건 부탁드립니다.\n"
+            f":k체크: *배정 받은 사물함 외 사용 불가*하오니 꼭! 배정 받은 사물함을 이용 부탁드립니다.\n"
+            f":k체크: 사물함 배정은 순차적으로 진행 되며, *사유불문 하고 변경 불가* 하오니 참고 부탁드립니다.\n"
             f"\n"
-            f"사우님들께서는 이점 숙지하시어 공용 냉장고 사용 부탁드립니다.\n"
+            f"\n"
+            f"사물함 배정 번호 등 자료는 스레드로 남겨드리오니 참고 부탁드리겠습니다.\n"
             f"\n"
             f"\n"
             f"*문의사항 : 인사총무팀 총무/시설 담당자*\n\n"
