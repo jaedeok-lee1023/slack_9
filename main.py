a5b2eab4ebfee0019e29f6029b1c47e62b98a043
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2024-12-03 18:24:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 # Slack Bot Token 및 채널 설정
 SLACK_BOT_TOKEN = "xapp-1-A07KTA1U18B-8111123509140-a63beec277745ec4b2a567686b86fa7cf609b7deafeab2634ade2d91e6938320"  # Slack Bot Token (xoxb로 시작)
-SLACK_CHANNEL = "슬랙-봇-테스트4"  # 전송할 채널 이름 (예: #general)
+SLACK_CHANNEL = "C083DC5L2V8"  # 전송할 채널 이름 (예: #general)
 FILE_PATH = "C:/Users/Kurly/Downloads/Kurly Introduction_국문_241119_aT_final.pdf"  # 전송할 파일 경로 (예: "C:/Users/Example/Documents/test.png")
 
 def upload_file_to_slack(file_path, channel, token):
